ba653684ea3f1a98bd6be6fb5c759f06d92ce736
visualizeua <visualizeua@gmail.com>
2017-04-11 20:27:00 -0500

    Update multiple_targets.js

--- multiple_targets.js
+++ multiple_targets.js
@@ -15,7 +15,7 @@ var World = {
 
         //COMMON STUFF FOR ALL LABS
 
-		var imgLogo = new AR.ImageResource("http://visualizeua.com/visualize.com.png");
+		var imgLogo = new AR.ImageResource("http://visualizeua.com/appAssets/common/visualize.com.png");
 
 		var LogoButton = new AR.ImageDrawable(imgLogo, 0.12, {
                       enabled: true,
@@ -24,7 +24,7 @@ var World = {
                       onClick: function LogoButtonClicked() {
                            LSSLVideo.pause();
                            LogoButton.clicked = true;
-                           AR.context.openInBrowser("http://visualizeua.wixsite.com/website",true);
+                           AR.context.openInBrowser("http://visualizeua.com/",true);
                            return true;
                       },
         			offsetX: 0.32,
@@ -33,7 +33,7 @@ var World = {
         });
 
 
-		var imgTeamPic = new AR.ImageResource("http://visualizeua.com/teamPic.JPG");
+		var imgTeamPic = new AR.ImageResource("http://visualizeua.com/appAssets/common/teamPic.JPG");
 
         var TeamPicOverlay = new AR.ImageDrawable(imgTeamPic, .203, {
             offsetX: 0,
@@ -44,7 +44,7 @@ var World = {
 
         //LSSL STUFF
 
-        var LSSLVideo = new AR.VideoDrawable("http://visualizeua.com/LSSLwikitude1080.mp4", 0.95, {
+        var LSSLVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/LSSL/LSSLwikitude1080shake.mp4", 0.95, {
 			offsetX: -0.15,
 			offsetY: +0.25,
 			isTransparent: true,
@@ -63,7 +63,7 @@ var World = {
 	    LSSLVideo.playing = true;
 
 
-		var playButtonImgLSSL = new AR.ImageResource("http://visualizeua.com/LSSLvidStill.png");
+		var playButtonImgLSSL = new AR.ImageResource("http://visualizeua.com/appAssets/LSSL/LSSLvidStill.png");
 
 		var playButtonLSSL = new AR.ImageDrawable(playButtonImgLSSL, 0.17, {
 			enabled: true,
@@ -98,7 +98,7 @@ var World = {
 
         //ECL STUFF
 
-        var ECLVideo = new AR.VideoDrawable("http://visualizeua.com/ECLwikitudeTommy1080.mp4", .95, {
+        var ECLVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/ECL/ECLwikitudeTommy.mp4", .95, {
 			offsetX: -0.15,
 			offsetY: +0.25,
 			isTransparent: true,
@@ -117,7 +117,7 @@ var World = {
 	    ECLVideo.playing = true;
 
 
-		var playButtonImgECL = new AR.ImageResource("http://visualizeua.com/ECLvidStill.png");
+		var playButtonImgECL = new AR.ImageResource("http://visualizeua.com/appAssets/ECL/ECLvidStill.png");
 
 		var playButtonECL = new AR.ImageDrawable(playButtonImgECL, 0.17, {
 			enabled: true,
@@ -152,7 +152,7 @@ var World = {
 
         //MFF STUFF
 
-        var MFFVideo = new AR.VideoDrawable("http://visualizeua.com/MFFwikitudeTommy1080.mp4", 0.95, {
+        var MFFVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/MFF/MFFgreenscreen.mp4", 0.95, {
 			offsetX: -0.15,
 			offsetY: +0.25,
 			isTransparent: true,
@@ -171,9 +171,26 @@ var World = {
 	    MFFVideo.playing = true;
 
 
+		var playButtonImgMFF = new AR.ImageResource("http://visualizeua.com/appAssets/MFF/MFFvidStill.png");
+
+		var playButtonMFF = new AR.ImageDrawable(playButtonImgMFF, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonMFFClicked() {
+                MFFVideo.pause();
+				playButtonMFF.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=zUDjzsqMaWs",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
         var pageThree = new AR.Trackable2DObject(this.tracker, "MFFTarget", {
 			drawables: {
-				cam: [TeamPicOverlay, LogoButton, MFFVideo]
+				cam: [TeamPicOverlay, LogoButton, MFFVideo, playButtonMFF]
 			},
             onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
 				MFFVideo.resume();
@@ -184,11 +201,448 @@ var World = {
 				MFFVideo.playing = false;
 			}
 		});
-
-
-
-
 	},
+
+
+
+        //LS Wind Tunnel STUFF
+
+        var LSWindTunnelVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/LSWindTunnel/lowSpeedgreenscreen.mp4", 0.95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function LSWindTunnelVideoClicked() {
+                if (LSWindTunnelVideo.playing) {
+            		LSWindTunnelVideo.pause();
+            		LSWindTunnelVideo.playing = false;
+            	} else {
+            		LSWindTunnelVideo.resume();
+            		LSWindTunnelVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    LSWindTunnelVideo.playing = true;
+
+
+		var playButtonImgLSSL = new AR.ImageResource("http://visualizeua.com/appAssets/LSWindTunnel/LSvidStill.png");
+
+		var playButtonLSWindTunnel = new AR.ImageDrawable(playButtonImgLSWindTunnel, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonLSWindTunnelClicked() {
+                LSWindTunnelVideo.pause();
+				playButtonLSWindTunnel.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=hPzU3HFEZdw",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageFour = new AR.Trackable2DObject(this.tracker, "WindTunnelTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, LSWindTunnelVideo, playButtonLSWindTunnel]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				LSWindTunnelVideo.resume();
+				LSWindTunnelVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				LSWindTunnelVideo.pause();
+				LSWindTunnelVideo.playing = false;
+			}
+		});
+
+
+
+        //Cube STUFF
+
+        var CubeVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/theCube/Cubegreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function CubeVideoClicked() {
+                if (CubeVideo.playing) {
+            		CubeVideo.pause();
+            		CubeVideo.playing = false;
+            	} else {
+            		CubeVideo.resume();
+            		CubeVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    CubeVideo.playing = true;
+
+
+		var playButtonImgCube = new AR.ImageResource("http://visualizeua.com/appAssets/theCube/CubevidStill.png");
+
+		var playButtonCube = new AR.ImageDrawable(playButtonImgCube, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonCubeClicked() {
+                CubeVideo.pause();
+				playButtonCube.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=TjWCpzexCBI",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageFive = new AR.Trackable2DObject(this.tracker, "CubeTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, CubeVideo, playButtonCube]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				CubeVideo.resume();
+				CubeVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				CubeVideo.pause();
+				CubeVideo.playing = false;
+			}
+		});
+
+
+
+        //Fenrir STUFF
+
+        var FenrirVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/Fenrir/Fenrirgreenscreen.mp4", 0.95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function FenrirVideoClicked() {
+                if (FenrirVideo.playing) {
+            		FenrirVideo.pause();
+            		FenrirVideo.playing = false;
+            	} else {
+            		FenrirVideo.resume();
+            		FenrirVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    FenrirVideo.playing = true;
+
+
+        var pageSix = new AR.Trackable2DObject(this.tracker, "FenrirTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, FenrirVideo]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				FenrirVideo.resume();
+				FenrirVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				FenrirVideo.pause();
+				FenrirVideo.playing = false;
+			}
+		});
+	},
+
+
+
+        //SPOT STUFF
+
+        var SPOTVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/SPOT/SPOTgreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function SPOTVideoClicked() {
+                if (SPOTVideo.playing) {
+            		SPOTVideo.pause();
+            		SPOTVideo.playing = false;
+            	} else {
+            		SPOTVideo.resume();
+            		SPOTVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    SPOTVideo.playing = true;
+
+
+		var playButtonImgSPOT = new AR.ImageResource("http://visualizeua.com/appAssets/SPOT/SPOTvidStill.png");
+
+		var playButtonSPOT = new AR.ImageDrawable(playButtonImgSPOT, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonSPOTClicked() {
+                SPOTVideo.pause();
+				playButtonSPOT.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=AmTCndvdvrU",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageSeven = new AR.Trackable2DObject(this.tracker, "SPOTTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, SPOTVideo, playButtonSPOT]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				SPOTVideo.resume();
+				SPOTVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				SPOTVideo.pause();
+				SPOTVideo.playing = false;
+			}
+		});
+
+
+
+        //Astrobotics STUFF
+
+        var AstroboticsVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/Astrobotics/Astroboticsgreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function AstroboticsVideoClicked() {
+                if (AstroboticsVideo.playing) {
+            		AstroboticsVideo.pause();
+            		AstroboticsVideo.playing = false;
+            	} else {
+            		AstroboticsVideo.resume();
+            		AstroboticsVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    AstroboticsVideo.playing = true;
+
+
+		var playButtonImgAstrobotics = new AR.ImageResource("http://visualizeua.com/appAssets/Astrobotics/AstroboticsvidStill.png");
+
+		var playButtonAstrobotics = new AR.ImageDrawable(playButtonImgAstrobotics, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonAstroboticsClicked() {
+                AstroboticsVideo.pause();
+				playButtonAstrobotics.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=26I-o7i66Fo",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageEight = new AR.Trackable2DObject(this.tracker, "AstroboticsTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, AstroboticsVideo, playButtonAstrobotics]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				AstroboticsVideo.resume();
+				AstroboticsVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				AstroboticsVideo.pause();
+				AstroboticsVideo.playing = false;
+			}
+		});
+
+
+
+        //Firefly STUFF
+
+        var FireflyVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/FireFly/Fireflygreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function FireflyVideoClicked() {
+                if (FireflyVideo.playing) {
+            		FireflyVideo.pause();
+            		FireflyVideo.playing = false;
+            	} else {
+            		FireflyVideo.resume();
+            		FireflyVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    FireflyVideo.playing = true;
+
+
+        var pageNine = new AR.Trackable2DObject(this.tracker, "FireflyTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, FireflyVideo]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				FireflyVideo.resume();
+				FireflyVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				FireflyVideo.pause();
+				FireflyVideo.playing = false;
+			}
+		});
+
+
+
+        //BamaRacing STUFF
+
+        var BamaRacingVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/BamaRacing/BamaRacinggreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function BamaRacingVideoClicked() {
+                if (BamaRacingVideo.playing) {
+            		BamaRacingVideo.pause();
+            		BamaRacingVideo.playing = false;
+            	} else {
+            		BamaRacingVideo.resume();
+            		BamaRacingVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    BamaRacingVideo.playing = true;
+
+
+		var playButtonImgBamaRacing = new AR.ImageResource("http://visualizeua.com/appAssets/BamaRacing/BamaRacingvidStill.png");
+
+		var playButtonBamaRacing = new AR.ImageDrawable(playButtonImgBamaRacing, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonBamaRacingClicked() {
+                BamaRacingVideo.pause();
+				playButtonBamaRacing.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=5wWb4450kZU",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageTen = new AR.Trackable2DObject(this.tracker, "BamaRacingTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, BamaRacingVideo, playButtonBamaRacing]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				BamaRacingVideo.resume();
+				BamaRacingVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				BamaRacingVideo.pause();
+				BamaRacingVideo.playing = false;
+			}
+		});
+
+
+
+        //CrimsonRacing STUFF
+
+        var CrimsonRacingVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/CrimsonRacing/CrimsonRacinggreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function CrimsonRacingVideoClicked() {
+                if (CrimsonRacingVideo.playing) {
+            		CrimsonRacingVideo.pause();
+            		CrimsonRacingVideo.playing = false;
+            	} else {
+            		CrimsonRacingVideo.resume();
+            		CrimsonRacingVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    CrimsonRacingVideo.playing = true;
+
+
+		var playButtonImgCrimsonRacing = new AR.ImageResource("http://visualizeua.com/appAssets/CrimsonRacing/CrimsonRacingvidStill.png");
+
+		var playButtonCrimsonRacing = new AR.ImageDrawable(playButtonImgCrimsonRacing, 0.17, {
+			enabled: true,
+			clicked: false,
+			zOrder: 3,
+			onClick: function playButtonCrimsonRacingClicked() {
+                CrimsonRacingVideo.pause();
+				playButtonCrimsonRacing.clicked = true;
+    			AR.context.openInBrowser("https://www.youtube.com/watch?v=D9DyjrPqrX0",true);
+    			return true;
+    		},
+			offsetY: -0.05,
+			offsetX: +0.23
+		});
+
+
+        var pageEleven = new AR.Trackable2DObject(this.tracker, "CrimsonRacingTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, CrimsonRacingVideo, playButtonCrimsonRacing]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				CrimsonRacingVideo.resume();
+				CrimsonRacingVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				CrimsonRacingVideo.pause();
+				CrimsonRacingVideo.playing = false;
+			}
+		});
+
+
+
+        //EcoCar STUFF
+
+        var EcoCarVideo = new AR.VideoDrawable("http://visualizeua.com/appAssets/EcoCar/EcoCargreenscreen.mp4", .95, {
+			offsetX: -0.15,
+			offsetY: +0.25,
+			isTransparent: true,
+			zOrder: 1,
+			onClick: function EcoCarVideoClicked() {
+                if (EcoCarVideo.playing) {
+            		EcoCarVideo.pause();
+            		EcoCarVideo.playing = false;
+            	} else {
+            		EcoCarVideo.resume();
+            		EcoCarVideo.playing = true;
+            	}
+            return true;
+            }
+		});
+	    EcoCarVideo.playing = true;
+
+
+        var pageTwelve = new AR.Trackable2DObject(this.tracker, "EcoCarTarget", {
+			drawables: {
+				cam: [TeamPicOverlay, LogoButton, EcoCarVideo]
+			},
+            onEnterFieldOfVision: function onEnterFieldOfVisionFn() {
+				EcoCarVideo.resume();
+				EcoCarVideo.playing = true;
+			},
+			onExitFieldOfVision: function onExitFieldOfVisionFn() {
+				EcoCarVideo.pause();
+				EcoCarVideo.playing = false;
+			}
+		});
+
+
+
+        //TornadoGun STUFF
+
 
 
 	worldLoaded: function worldLoadedFn() {
